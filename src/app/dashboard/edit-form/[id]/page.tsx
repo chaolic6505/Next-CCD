5b814dc2ed0d357a204afdcdbc195568ceb3f250
Steven <chaolic6505@gmail.com>
2024-06-05 22:33:52 -0700

    format

--- src/app/dashboard/edit-form/[id]/page.tsx
+++ src/app/dashboard/edit-form/[id]/page.tsx
@@ -6,17 +6,17 @@ import FormDetails from "./FormDetails";
 import FormResponses from "./FormResponses";
 
 export default function Page({ params }: { params: { id: string } }) {
-	return (
-		<>
-			<Unauthenticated>Please sign in.</Unauthenticated>
-			<Authenticated>
-				<FormDetails id={params.id} />
-				<hr />
-				<FormFields id={params.id} />
-				<hr />
-				<h2>Responses</h2>
-				<FormResponses id={params.id} />
-			</Authenticated>
-		</>
-	);
+  return (
+    <>
+      <Unauthenticated>Please sign in.</Unauthenticated>
+      <Authenticated>
+        <FormDetails id={params.id} />
+        <hr />
+        <FormFields id={params.id} />
+        <hr />
+        <h2>Responses</h2>
+        <FormResponses id={params.id} />
+      </Authenticated>
+    </>
+  );
 }
