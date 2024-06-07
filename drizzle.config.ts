06565776cbc8987a42c3dfbce82076339816cbab
Steven <chaolic6505@gmail.com>
2024-06-06 23:39:27 -0700

    add neon db

---
+++ drizzle.config.ts
@@ -0,0 +1,15 @@
+import * as dotenv from 'dotenv';
+import { defineConfig } from "drizzle-kit";
+
+dotenv.config({
+    path: '.env.local',
+});
+
+export default defineConfig({
+    out: "./drizzle",
+    dialect: "postgresql",
+    schema: "./db/schema.ts",
+    dbCredentials: {
+        url: process.env.DATABASE_URL!,
+    },
+});
